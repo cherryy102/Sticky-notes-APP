330e408d7785269e01cad26124ba869241b33d2c
Damian Wiśniowski <dkontow@gmail.com>
2021-04-28 12:55:50 +0200

    save poition for multiple note

--- js/main.js
+++ js/main.js
@@ -4,21 +4,25 @@ const notesBar = document.querySelectorAll('.notes');
 
 let savePositionX;
 let savePositionY;
-notesBar.forEach(noteBar => {
-    if (JSON.parse(localStorage.getItem('notesPositionX')) != null) {
-        savePositionX = JSON.parse(localStorage.getItem('notesPositionX'));
-        savePositionY = JSON.parse(localStorage.getItem('notesPositionY'))
-        noteBar.style.top = `${savePositionY[0]}px`;
-        noteBar.style.left = `${savePositionX[0]}px`;
+if (JSON.parse(localStorage.getItem('notesPositionX')) != null) {
+    savePositionX = JSON.parse(localStorage.getItem('notesPositionX'));
+    savePositionY = JSON.parse(localStorage.getItem('notesPositionY'))
+    for (let i = 0; i < savePositionX.length; i++) {
+        notesBar[i].style.top = `${savePositionY[i]}px`;
+        notesBar[i].style.left = `${savePositionX[i]}px`;
     }
-})
+}
+//define variables
 let active = false;
 let notesX = [];
 let notesY = [];
+let notesContent = [];
 let insertX;
 let insertY;
 let moveNote;
-//funkcja sprawdza czy mozna uzywac localStorage
+let idNotes = [];
+let index;
+//function check if localStorage can be used
 function localStorageTest() {
     const test = "test" + new Date().valueOf();
     try {
@@ -29,34 +33,46 @@ function localStorageTest() {
         return false;
     }
 }
+notesBar.forEach(id => {
+    idNotes.push(id.dataset.note)
+})
 const startMove = function(e) {
     active = !active;
-    moveNote = document.querySelector(`[data-note='${this.getAttribute('data-note')}'] `)
+    moveNote = document.querySelector(`[data-note='${this.dataset.note}'] `)
+
+    dataNote = this.dataset.note;
     insertX = e.offsetX;
     insertY = e.offsetY;
 }
 const moving = function(e) {
     if (active) {
-        notesX[0] = e.clientX - insertX;
-        notesY[0] = e.clientY - insertY;
-        moveNote.style.top = `${notesY[0]}px`
-        moveNote.style.left = `${notesX[0]}px`
+        index = idNotes.indexOf(dataNote);
+
+        notesX[index] = e.clientX - insertX;
+        notesY[index] = e.clientY - insertY;
+        if (savePositionX) {
+            for (let i = 0; i < savePositionX.length; i++) {
+                if (notesX[i] == null) {
+                    notesX[i] = savePositionX[i];
+                    notesY[i] = savePositionY[i];
+                }
+            }
+        }
+        moveNote.style.top = `${notesY[index]}px`
+        moveNote.style.left = `${notesX[index]}px`
     }
 }
 const endMove = function(e) {
         active = !active;
-        //sprawdzanie czy mozna korzystac z localstorage
+        //if localStorage can be used create variable
         if (localStorageTest()) {
             localStorage.setItem('notesPositionX', JSON.stringify(notesX));
             localStorage.setItem('notesPositionY', JSON.stringify(notesY));
         }
     }
-    //wywolanie
+    //function call
 notesBar.forEach(noteBar => {
     noteBar.addEventListener('mousedown', startMove);
     noteBar.addEventListener('mouseup', endMove)
 })
 document.addEventListener('mousemove', moving)
-
-
-//save text
